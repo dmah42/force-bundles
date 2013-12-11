6ff240b3db0e8fe191a5124c9eaef491f56d0b1e
Dominic Hamon <dominic+github@google.com>
2013-12-10 17:03:22 -0800

    Initial version
    Basic testing suggests that it runs and does something but it is unclear if it
    is the right thing yet.

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ force-bundles
 =============
 
 Force directed edge bundling for graph visualization: http://www.win.tue.nl/~dholten/papers/forcebundles_eurovis.pdf
+
+    $ g++ -Wall -Werror -o force-bundles *.cc
+    $ ./force-bundles < ./test_graph.txt
---
+++ graph.cc
@@ -0,0 +1,35 @@
+#include "graph.h"
+
+void Graph::Subdivide(int segments) {
+  for (size_t i = 0; i < edges_.size(); ++i) {
+    edges_[i].Subdivide(segments);
+  }
+}
+
+bool Graph::Step(double dt) {
+  for (size_t i = 0; i < edges_.size(); ++i) {
+    Edge& p = edges_[i];
+    p.ClearForces();
+    for (size_t j = 0; j < edges_.size(); ++j) {
+      if (i == j)
+        continue;
+      const Edge& q = edges_[j];
+      p.AddForces(q);
+    }
+  }
+
+  // Set done to false if any node moves > EPSILON
+  bool done = true;
+  for (size_t i = 0; i < edges_.size(); ++i) {
+    if (edges_[i].UpdatePositions(dt))
+      done = false;
+  }
+  return done;
+}
+
+void Graph::Print() const {
+  for (size_t i = 0; i < edges_.size(); ++i) {
+    std::cout << i << ": " << edges_[i] << "\n";
+  }
+}
+
